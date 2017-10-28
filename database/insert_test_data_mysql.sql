c8b06e42c5bf036507abc256c3b81715ec4cd5be
Juslin Jukka <jusju@haaga-helia.fi>
2017-10-28 15:20:35 +0300

    eka kommitti

---
+++ database/insert_test_data_mysql.sql
@@ -0,0 +1,4 @@
+-- MUISTA! console encoding latin1
+
+INSERT INTO Osoitteet(nimi, katuosoite, postinumero, postitoimipaikka, osavaltio, maa) 
+VALUES('Sakari ja Leila Lukkarinen','Ojahaantie 8 B 28','01600','Vantaa','Uusimaa','FINLAND');
